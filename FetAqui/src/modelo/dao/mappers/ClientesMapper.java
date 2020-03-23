e3f4a3c7c46dbe3745ca91bf2d095b99812ed599
Ramón Horrach Martin <rhorrachmartin@gmail.com>
2020-03-23 17:47:24 +0100

    Incluimos la select en el mapper de cliente para recuperar el historial de pedidos.

--- FetAqui/src/modelo/dao/mappers/ClientesMapper.java
+++ FetAqui/src/modelo/dao/mappers/ClientesMapper.java
@@ -1,9 +1,12 @@
 package modelo.dao.mappers;
 
 
+import java.util.ArrayList;
+
 import org.apache.ibatis.annotations.Param;
 
 import modelo.pojo.Cliente;
+import modelo.pojo.PedidoDetallado;
 
 /**
  * Interfaz mapper para manejar los mapper de Clientes
@@ -14,12 +17,19 @@ import modelo.pojo.Cliente;
 public interface ClientesMapper {
 
 	/**
-	 * Método para obtener los usuarios
+	 * Método para obtener un cliente
 	 * 
 	 * @param email   Espera recibir String con el email del cliente
 	 * @param password Espera recibir String con el password del cliente
 	 * @return Devuelve un pojo Cliente
 	 */
 	public Cliente getCliente(@Param("email") String email, @Param("password") String password);
+	
+	/**
+	 * Método para obtener todos los pedidos de un cliente para su historial
+	 * @param id_cliente recibe el id del cliente
+	 * @return devuelve un arraylist de PedidoDetallado
+	 */
+	public ArrayList<PedidoDetallado> getPedidos(@Param("id_cliente") Integer id_cliente);
 
 }
