1a9765eb53b723587f601555256df4317b699511
Ramón Horrach Martin <rhorrachmartin@gmail.com>
2020-03-23 15:36:56 +0100

    Mapper de direcciones por poblacion

---
+++ FetAqui/src/modelo/dao/mappers/DireccionesMapper.java
@@ -0,0 +1,24 @@
+package modelo.dao.mappers;
+
+
+import java.util.ArrayList;
+
+import org.apache.ibatis.annotations.Param;
+
+import modelo.pojo.Direccion;
+
+/**
+ * Interfaz mapper para manejar los mapper de Direcciones
+ * 
+ * @author ramon
+ *
+ */
+public interface DireccionesMapper {
+
+	/**
+	 * Método para obtener las direcciones de una población
+	 * @return ArrayList<Direccion> 
+	 */
+	public ArrayList<Direccion> getDireccionesPorPoblacion(@Param("id_poblacion") Integer id_poblacion);
+
+}
--- FetAqui/src/modelo/pojo/Direccion.java
+++ FetAqui/src/modelo/pojo/Direccion.java
@@ -4,16 +4,36 @@ public class Direccion {
 	
 	Integer id;
 	String direccion;
-	Integer poblacion;
+	Integer id_poblacion;
+	String poblacion;
 	
 	public Direccion() {
 		
 	}
 	
-	public Direccion(Integer id, String direccion, Integer poblacion) {
+	public Direccion(Integer id, String direccion, Integer id_poblacion, String poblacion) {
 		super();
 		this.id = id;
 		this.direccion = direccion;
+		this.id_poblacion = id_poblacion;
+		this.poblacion = poblacion;
+	}
+	
+	
+
+	public Integer getId_poblacion() {
+		return id_poblacion;
+	}
+
+	public void setId_poblacion(Integer id_poblacion) {
+		this.id_poblacion = id_poblacion;
+	}
+
+	public String getPoblacion() {
+		return poblacion;
+	}
+
+	public void setPoblacion(String poblacion) {
 		this.poblacion = poblacion;
 	}
 
@@ -33,13 +53,7 @@ public class Direccion {
 		this.direccion = direccion;
 	}
 
-	public Integer getPoblacion() {
-		return poblacion;
-	}
-
-	public void setPoblacion(Integer poblacion) {
-		this.poblacion = poblacion;
-	}
+	
 	
 	
 
